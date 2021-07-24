9bb6fb333bc14468ddd59977a7915fa7996c9c10
asmithasatesh <asmithasatesh@gmail.com>
2021-07-24 15:35:29 +0530

    [Asmitha]Add Person Details to a List

--- PersonDataManagement/PersonDetailManagement.cs
+++ PersonDataManagement/PersonDetailManagement.cs
@@ -13,12 +13,21 @@ namespace PersonDataManagement
             //List to store person details
             List<Person> PeopleList = new List<Person>();
             PeopleList.Add(new Person(2,"Harsha","tn",17));
-            PeopleList.Add(new Person(1,"Indu","up",22));
-            PeopleList.Add(new Person(8,"Akshaya","punjab",44));
-            PeopleList.Add(new Person(21,"Arpita","bihar",77));
-            PeopleList.Add(new Person(5,"Megha","kerala",62));
-            PeopleList.Add(new Person(4,"Ruju","Banglore",22));
+            PeopleList.Add(new Person(1, "Indu", "up", 22));
+            PeopleList.Add(new Person(8, "Akshaya", "punjab", 44));
+            PeopleList.Add(new Person(21, "Arpita", "bihar", 77));
+            PeopleList.Add(new Person(5, "Megha", "kerala", 62));
+            PeopleList.Add(new Person(4, "Ruju", "Banglore", 22));
+            DisplayDetails(PeopleList);
 
+        }
+        //Method to display details
+        public static void DisplayDetails(List<Person> peopleList)
+        {
+            foreach (Person person in peopleList)
+            {
+                Console.WriteLine("SSN: {0} Name: {1} Address: {2} Age: {3}  ", person.SSN, person.name, person.address, person.age);
+            }
         }
     }
 }
